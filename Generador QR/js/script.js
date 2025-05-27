bc3e2bf169a23881a36680f837f86d3dacff8014
AnteroTeobaldo <162181896+AnteroTeobaldo@users.noreply.github.com>
2025-05-27 15:08:32 -0500

    Actualizar Generador QR/js/script.js

--- Generador QR/js/script.js
+++ Generador QR/js/script.js
@@ -1,6 +1,6 @@
 function generarQR() {
     const texto = document.getElementById("texto").value;
-    const qr = qrcode(0, 'M'); // Tipo y corrección de error
+    const qr = qrcode(0, 'M'); 
     qr.addData(texto);
     qr.make();
     document.getElementById("qr").innerHTML = qr.createSvgTag({ cellSize: 8 });
