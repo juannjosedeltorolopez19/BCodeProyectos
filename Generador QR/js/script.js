06c827c68565f2cbb07fa29c1405a23c4a50d71c
AnteroTeobaldo <162181896+AnteroTeobaldo@users.noreply.github.com>
2025-06-06 18:42:31 -0500

    Delete Generador QR directory

--- Generador QR/js/script.js
+++
@@ -1,7 +0,0 @@
-function generarQR() {
-    const texto = document.getElementById("texto").value;
-    const qr = qrcode(0, 'M'); 
-    qr.addData(texto);
-    qr.make();
-    document.getElementById("qr").innerHTML = qr.createSvgTag({ cellSize: 8 });
-}
