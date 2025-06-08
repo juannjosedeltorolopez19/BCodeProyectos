88850b81425e2f48901d62ae05f0e68eb42ec92c
AnteroTeobaldo <162181896+AnteroTeobaldo@users.noreply.github.com>
2025-06-07 20:10:33 -0500

    Subir carpeta JSjs

---
+++ JS/Script.js
@@ -0,0 +1,9 @@
+.Love__You{
+    float: left;
+    width: 448px;
+    margin - top: 60px!important;
+    height: 480px;
+    color: #333;
+    font - family: "Love";
+    font - size: 25px;
+}rgba(101, 57, 165, 0.8)rgba(165, 57, 74, 0.8)rgba(255, 245, 246, 0.8)rgba(0, 0, 0, 0.8)ya 
