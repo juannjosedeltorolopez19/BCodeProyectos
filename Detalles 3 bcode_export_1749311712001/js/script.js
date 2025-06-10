b45abe3004d5fdf4d5fb927a9a3683dfc547089a
AnteroTeobaldo <162181896+AnteroTeobaldo@users.noreply.github.com>
2025-06-10 10:49:09 -0500

    Actualizar Detalles 3 bcode_export_1749311712001/js/script.js

--- Detalles 3 bcode_export_1749311712001/js/script.js
+++ Detalles 3 bcode_export_1749311712001/js/script.js
@@ -2,7 +2,7 @@
 
 // Configuración Matrix
 const LINE_COUNT = 30;
-const MATRIX_TEXT = "TE QUIERO ALE ";
+const MATRIX_TEXT = "TE QUIERO";
 const LOVE_CHARS = ["❤", "🌹", "💕", "🌹", "💝"];
 
 // Elementos interactivos
