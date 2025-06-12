c31214b8a50ee4be1f811607356fdf36af4891da
AnteroTeobaldo <162181896+AnteroTeobaldo@users.noreply.github.com>
2025-06-12 09:38:03 -0500

    mm

--- Te amo mi príncipe/js/script.js
+++ Te amo mi príncipe/js/script.js
@@ -1,6 +1,6 @@
 // Configuración Matrix
 const LINE_COUNT = 30;
-const MATRIX_TEXT = "TE AMO MI REINA HERMOSA";
+const MATRIX_TEXT = "TE AMO MI REY HERMOSO";
 const LOVE_CHARS = [""];
 
 // Inicializar Matrix
