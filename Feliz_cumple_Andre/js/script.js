13e2d9f3224d7f805c47c53f50c8f2ceb0b89447
AnteroTeobaldo <162181896+AnteroTeobaldo@users.noreply.github.com>
2025-07-08 05:57:40 +0200

    Eliminar Feliz_cumple_Andre/js/script.js después de mover

--- Feliz_cumple_Andre/js/script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-// Reproduce automáticamente la canción al cargar
-document.addEventListener("DOMContentLoaded", () => {
-  const audio = document.getElementById("backgroundAudio");
-    audio.volume = 0.4;
-    });
