6c1c61c510f109819a40d680d69069147f0dddc0
AnteroTeobaldo <162181896+AnteroTeobaldo@users.noreply.github.com>
2025-07-29 20:11:14 -0500

    Renombrar Feliz_cumple_Freyci/js/script.js a Feliz_cumple/js/script.js

---
+++ Feliz_cumple/js/script.js
@@ -0,0 +1,6 @@
+// script.js
+// Reproduce automáticamente la canción al cargar
+document.addEventListener("DOMContentLoaded", () => {
+  const audio = document.getElementById("backgroundAudio");
+    audio.volume = 0.4;
+    });
