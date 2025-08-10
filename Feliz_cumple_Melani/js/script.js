a1c9ce0b169ffca104ed421635dab4548c2d357a
BCode Pro <162181896+AnteroTeobaldo@users.noreply.github.com>
2025-08-10 13:52:20 -0500

    Actualizar Feliz_cumple_Melani/js/script.js

--- Feliz_cumple_Melani/js/script.js
+++ Feliz_cumple_Melani/js/script.js
@@ -1,6 +1,1 @@
-// script.js
-// Reproduce automáticamente la canción al cargar
-document.addEventListener("DOMContentLoaded", () => {
-  const audio = document.getElementById("backgroundAudio");
-    audio.volume = 0.4;
-    });
+undefined
